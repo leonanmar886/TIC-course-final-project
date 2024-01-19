44165556b190058e33bfadf604950ee94b807906
Leonan Marques <leonan.marques886@gmail.com>
2024-01-18 21:53:48 -0300

    frontend have been initialized

--- app/src/main/java/com/example/restaurantmenu/MainActivity.kt
+++ app/src/main/java/com/example/restaurantmenu/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.restaurantmenu
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContent {
+      RestaurantMenuApp()
+    }
   }
+}
+
+@Composable
+fun RestaurantMenuApp() {
+
 }
