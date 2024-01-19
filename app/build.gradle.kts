62daec2d9fff3459e097dce76ca61560b2c954ad
Leonan Marques <leonan.marques886@gmail.com>
2024-01-18 21:53:12 -0300

    dependencies have been updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,13 @@ android {
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
+  buildFeatures {
+    compose = true
+  }
+  composeOptions {
+    kotlinCompilerExtensionVersion = "1.4.7"
+  }
+
   buildTypes {
     release {
       isMinifyEnabled = false
@@ -37,7 +44,17 @@ dependencies {
   implementation("androidx.core:core-ktx:1.12.0")
   implementation("androidx.appcompat:appcompat:1.6.1")
   implementation("com.google.android.material:material:1.9.0")
-  implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+  implementation(platform("androidx.compose:compose-bom:2023.05.01"))
+  implementation("androidx.activity:activity-compose:1.7.2")
+  implementation("androidx.compose.material:material-icons-extended")
+  implementation("androidx.compose.material3:material3")
+  implementation("androidx.compose.ui:ui")
+  implementation("androidx.compose.ui:ui-graphics")
+  implementation("androidx.compose.ui:ui-tooling-preview")
+  implementation("androidx.core:core-ktx:1.10.1")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+  implementation("androidx.compose.material:material-icons-extended")
+
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
